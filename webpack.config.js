0168fe4ff5438aad6349a000096a463841be218f
fresonn <vladislavs319@gmail.com>
2023-12-04 16:44:11 +0200

    public folder for local development

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Development',
-      template: 'src/index.html'
+      template: 'public/index.html'
     })
   ],
   resolve: {
