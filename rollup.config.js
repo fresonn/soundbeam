632c366d9565fe467b23e22ede5e248161b0e19b
fresonn <vladislavs319@gmail.com>
2023-12-05 15:04:28 +0200

    npm:dev script as "sandbox"

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,6 @@
 import typescript from '@rollup/plugin-typescript'
 import commonjs from '@rollup/plugin-commonjs'
-import terser from '@rollup/plugin-terser'
-import serve from 'rollup-plugin-serve'
-// import html from '@rollup/plugin-html'
+// import terser from '@rollup/plugin-terser's
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 /**
@@ -10,27 +8,15 @@ import { nodeResolve } from '@rollup/plugin-node-resolve'
  */
 export default {
   input: 'src/index.ts',
-  // external: ['howler'],
   output: {
     file: 'output/index.js',
-    format: 'cjs'
+    format: 'es',
+    name: 'Soundbeam'
   },
   plugins: [
     typescript(),
     commonjs(),
-    nodeResolve(),
     // terser(),
-    serve({
-      historyApiFallback: '/public/index.html',
-      port: 4000,
-      contentBase: ['public', 'output', 'assets'],
-      onListening: function (server) {
-        const address = server.address()
-        const host = address.address === '::' ? 'localhost' : address.address
-        // by using a bound function, we can access options as `this`
-        const protocol = this.https ? 'https' : 'http'
-        console.log(`Server listening at ${protocol}://${host}:${address.port}/`)
-      }
-    })
+    nodeResolve()
   ]
 }
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,3 @@
 import Soundbeam from './soundbeam'
 
-const nodeEl = document.getElementById('player') as HTMLElement
-
-const player = new Soundbeam({
-  node: nodeEl
-})
-
-console.log(player)
-
-const playBtn = document.getElementById('play')
-const pauseBtn = document.getElementById('pause')
-
-player.load('./main.mp3')
-
-if (playBtn && pauseBtn) {
-  playBtn.addEventListener('click', () => {
-    player.play()
-  })
-
-  pauseBtn.addEventListener('click', () => {
-    player.pause()
-  })
-}
+export default Soundbeam
